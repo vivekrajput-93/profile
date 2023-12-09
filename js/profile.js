5dad6b7f5dc52a232d13c63eeb5a38daf158d8d9
vivekrajput-93 <viveksinghraajputt@gmail.com>
2023-12-09 15:47:49 +0530

    Login Completed

--- js/profile.js
+++ js/profile.js
@@ -1,18 +1,35 @@
 $(document).ready(function() {
+    // Get the email from somewhere, replace 'user@example.com' with the actual email
+    let data = localStorage.getItem("userData")
+
+    console.log(data.email);
+    console.log(data)
+    console.log(JSON.parse(data));
+    var userEmail = {
+        userEmail : JSON.parse(data).email
+
+    }; // Replace this with the actual email or get it dynamically
+
+
+
     $.ajax({
         url: "../php/profile.php",
         type: "GET",
         dataType: "json",
+        data : userEmail,
         success: function(response) {
-            if(response.message == "successfull") {
+            console.log(response)
+            if (response.message == 'success') {
                 // Populate user data in the profile page
-                $("#username").text(response.data.username);
-                $("#email").text(response.data.email);
-                $("#age").text(response.data.age);
-                $("#dob").text(response.data.dob);
-                $("#contact").text(response.data.contact);
+                var userData = response.data;
+                // Update your HTML elements with the user data
+                $('#username').text(userData.username);
+                $('#email').text(userData.email);
+                $('#age').text(userData.age);
+                $('#dob').text(userData.dob);
+                $('#contact').text(userData.contact);
             } else {
-                console.error("Failed to fetch user data");
+                console.error(response.message)
             }
         },
         error: function(xhr, status, error) {
--- php/profile.php
+++ php/profile.php
@@ -1,42 +1,12 @@
 <?php
-require_once '../vendor/autoload.php';
+
 
 // MySQL Connection
-$server = "localhost";
-$serverusername = "root";
-$serverpassword = "viveksingh__1729";
-$dbname = "vivek-auth";
-
-$conn = new mysqli($server, $serverusername, $serverpassword, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Assuming session has been started
-session_start();
-
-if (!isset($_SESSION['user_email'])) {
-    echo json_encode(['message' => 'Unauthenticated']);
-    exit;
-}
-
-$email = $_SESSION['user_email'];
-
-// Retrieve user data based on email from MySQL
-$sql = "SELECT * FROM users WHERE email = '$email'";
-$result = $conn->query($sql);
-
-if ($result && $result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-
-    // Get the user's email
-    $userEmail = $row['email'];
+if (isset($_GET['userEmail'])) {
+    $userEmail = $_GET['userEmail'];
 
 
-
-
-    // MongoDB Connection
+    require_once '../vendor/autoload.php';
     $database = new MongoDB\Client('mongodb://localhost:27017');
     $myDatabase = $database->profile;
     $userCollection = $myDatabase->users;
@@ -47,15 +17,15 @@ if ($result && $result->num_rows > 0) {
     if ($userData) {
         // Return user data as JSON
         header('Content-Type: application/json');
-        echo json_encode(['message' => 'successfull', 'data' => $userData]);
+        echo json_encode(['message' => 'success', 'data' => $userData]);
     } else {
         // Return failure message if user data isn't found in MongoDB
-        echo json_encode(['message' => 'No user found in MongoDB']);
+        echo json_encode(['message' => $userEmail]);
     }
 } else {
     // Return failure message if user email isn't found in MySQL
     echo json_encode(['message' => 'No user found in MySQL']);
 }
 
-$conn->close();
+
 ?>
